5bb21ab9b73e987a82f43a33851b22b814e83996
offshoreCerag <57848264+offshoreCerag@users.noreply.github.com>
2021-08-07 00:15:57 +0530

    db added

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,16 +1,16 @@
  //Server Details
-/*module.exports = {
+module.exports = {
     connectionLimit: 10,
     host: 'localhost',
-    user: 'voltaicb_test',
-    password: '&cW-B$.8)Bfh',
-    database: 'voltaicb_test',
+    user: 'aspireme_db',
+    password: 'fu32iNQfp1g#',
+    database: 'aspireme_db',
     multipleStatements: true,
     port     : '3306'
 }
-*/
+
 //Local Details
-module.exports = {
+/*module.exports = {
     connectionLimit: 10,
     host: '127.0.0.1',
     user: 'root',
@@ -18,4 +18,4 @@ module.exports = {
     database: 'aspire_membership',
     multipleStatements: true,
     port     : '3306'
-}
+}*/
